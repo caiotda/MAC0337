582b1df4b5451184776db5d556f70686388df64a
Caio Andrade <caiotdandrade@gmail.com>
2020-12-08 16:52:41 -0300

    [EP3a] Termina EP, documenta codigo e adiciona testes]

--- EPs/EP3/parte_1/karplusstrong~.lua
+++ EPs/EP3/parte_1/karplusstrong~.lua
@@ -1,20 +1,37 @@
+--Nome: Caio Túlio de Deus Andrade
+--NUSP: 9797232
+
+
 function ofelia.list(a);
 ;
+    --Taxa de amostragem do PD
     R = 44100
+    -- Frequencia central em hz
     F = a[1]
+    -- Duração da nota, em segundos
     D = a[2]
-    M.duration = math.floor(D*R + 0.5)
 
-    print(R, F)
+    M.duration = math.floor(D*R + 0.5)
+    -- Variavel utilizada para iterarmos pela tabela ks
     M.i = 1
-    M.blocks = 1
+
+    -- Numero de blocos processados
+    M.blocks = 0
     M.output = ofTable()
+
+    -- Tamanho da tabela ks
     M.L = math.floor(R/F)
     M.ks = ofTable()
+
+    -- Fator de decaimentos das ultimas dez notas. Sera decrescido num fator de
+    -- 0.1
     M.alpha = 1
+
+    -- Inicialização da tabela ks com valores aleatórios
     for j = 1, M.L do;
         M.ks[j] = 2*math.random() - 1;
     end;
+
     M.ant = M.ks[(-1)%M.L + 1]
     return M.ks
 end;
