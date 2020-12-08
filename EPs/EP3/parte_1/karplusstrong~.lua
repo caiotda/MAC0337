2a09a8ccfea58f4bad91b403dadde907d11df31a
Caio Andrade <caiotdandrade@gmail.com>
2020-12-08 11:29:27 -0300

    [EP3a] Faz setup no ofelia.list da tabela Ks e duração da sintese

--- EPs/EP3/parte_1/karplusstrong~.lua
+++ EPs/EP3/parte_1/karplusstrong~.lua
@@ -1,5 +1,19 @@
 function ofelia.list(a);
 ;
-    print(a[1]);
-    print(a[2]);
+    R = 44100
+    F = a[1]
+    D = a[2]
+
+    len = math.floor(D * R + 0.5)
+    print(R, F)
+    M.L = math.floor(R/F)
+    M.ks = ofTable()
+    for i = 1, M.L do;
+        M.ks[i] = 2*math.random() - 1;
+    end;
+    return M.ks
+end;
+
+function ofelia.perform();
+    print(M.ks)
 end;
