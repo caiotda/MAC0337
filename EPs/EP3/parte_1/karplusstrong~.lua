7b7ef79766623eaded6e3b058a51b9feb212eaeb
Caio Andrade <caiotdandrade@gmail.com>
2020-12-08 16:13:12 -0300

    [EP3A] Aplica filtro de passa baixa e gera som de cordas

--- EPs/EP3/parte_1/karplusstrong~.lua
+++ EPs/EP3/parte_1/karplusstrong~.lua
@@ -6,26 +6,28 @@ function ofelia.list(a);
     M.duration = math.floor(D*R + 0.5)
 
     print(R, F)
-    M.i = 0
-    M.blocks = 0
+    M.i = 1
+    M.blocks = 1
     M.output = ofTable()
     M.L = math.floor(R/F)
     M.ks = ofTable()
     for j = 1, M.L do;
         M.ks[j] = 2*math.random() - 1;
     end;
+    M.ant = M.ks[(-1)%M.L + 1]
     return M.ks
 end;
 
 function ofelia.perform();
     if M.blocks < M.duration then;
-        print(M.i)
         for n=1, 64 do;
             -- Etapa de construção da saida. --
             M.output[n] = M.ks[M.i];
             -- Etapa de atualização da tabela. --
             --M.ant = ; -- Armazena o x[n] antes de aplicar o filtro da media
-            --M.ks[M.i] = 0.5*(M.ks[M.i]) + M.ks[(M.i-2)%M.L + 1]);
+            aux = M.ks[M.i]
+            M.ks[M.i] =  (M.ks[M.i] + M.ant)/2;
+            M.ant = aux
             M.i = M.i%M.L + 1;
             M.blocks = M.blocks + 1
         end;
